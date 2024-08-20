c22de11ad5755b4fba7739697f4de0c3c86f7daf
JakubRumpca <jakub.rumpca@gmail.com>
2024-08-20 09:28:58 +0200

    headless: true

--- playwright.config.js
+++ playwright.config.js
@@ -3,8 +3,10 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   reporter: 'html',
+  retries: 2,
   use: {
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    headless: true,
   },
 
 });
